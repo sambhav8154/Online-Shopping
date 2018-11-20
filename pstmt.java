f6315fb6a3a5ef52acf6af1126f18d6810227dc5
sambhav8154 <45209257+sambhav8154@users.noreply.github.com>
2018-11-20 22:57:42 +0530

    Add files via upload

---
+++ pstmt.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hp
+ */
+class pstmt {
+
+    static void setString(int i, String s) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static int executeUpdate() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
